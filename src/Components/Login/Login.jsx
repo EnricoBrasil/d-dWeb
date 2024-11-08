5cc2bdfefe138664d1e5e35261bc4eab8b8c7339
enrico brasil <enrico.brasil@prit.app>
2024-11-08 16:14:54 -0300

    first commit

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,52 @@
+import { FaUser, FaLock } from "react-icons/fa";
+
+import { useState } from "react";
+import "./Login.css";
+
+const Login = () => {
+    const [username, setUsename] = useState("");
+    const [password, setPassword] = useState("");
+
+    const hacndleSubmit = (event) => {
+        event.preventDefault();
+        alert("Enviando os dados: " +username + " - " +password);
+    };
+
+  return (
+    <div className="container">
+      <form onSubmit={hacndleSubmit}>
+        <h1>Acesse o sitema</h1>
+        <div>
+          <input type="email" placeholder="E-mail" 
+          onChange={(e) => setUsename(e.target.value)}/>
+          <FaUser className="icon"/>
+        </div>
+        
+        <div>
+          <input type="password" placeholder="Senha" 
+          onChange={(e) => setPassword(e.target.value)}/>
+          <FaLock className="icon"/>
+        </div>
+
+        <div className="recall-forget">
+            <label>
+                <input type="checkbox" />
+                Lembrar de mim!
+            </label>
+            <a href="#"> Esqueceu a senha?</a>
+        </div>
+
+        <button>Entar</button>
+
+        <div className="signup-link">
+            <p>
+                Não tem uma conta? <a href="#">Registrar</a>
+            </p>
+
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
